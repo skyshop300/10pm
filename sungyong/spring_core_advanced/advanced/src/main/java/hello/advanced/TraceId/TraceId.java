47d8189bdc03d70ae1d602e888cf7b769e8cc58a
박성용 <syp@syp.local>
2022-08-31 22:35:43 +0900

    sy - 20220831

---
+++ sungyong/spring_core_advanced/advanced/src/main/java/hello/advanced/TraceId/TraceId.java
@@ -0,0 +1,27 @@
+package hello.advanced.TraceId;
+
+import java.util.UUID;
+
+public class TraceId {
+    private String id;
+    private int level;
+
+    public TraceId() {
+        this.id = createId();
+        this.level = 0;
+    }
+
+    private TraceId(String id, int level) {
+        this.id = id;
+        this.level = level;
+    }
+
+    private String createId() {
+        return UUID.randomUUID().toString().substring(0, 8);
+    }
+
+    private TraceId createNextId() {
+        return new TraceId(id, level + 1);
+    }
+
+}
